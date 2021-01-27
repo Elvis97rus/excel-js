f3a54a8623851b5e6f092e2766a2cc31ab3859cb
elvis97rus <elvis97rus@gmail.com>
2021-01-27 15:03:40 +0300

    Resize Functional for colls & rows - create resize html el's & styles - add rows'n'colls meta-data - handle onMouseDown / onMouseMove / onMouseUp events - resize cols & rows realisation - fixed onmousemove performance problems - calculate/change width/height only onMouseUp - add blue resizer border move onMouseMove - enhance performance onMouseMove, shorten resizer code - move resize function to the separate table file

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
-import {$} from '@core/dom';
+import {resize} from '@/components/table/table.resize';
+import {shouldResize} from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -16,76 +17,8 @@ export class Table extends ExcelComponent {
     }
 
     onMousedown(event) {
-        if (event.target.dataset.resize) {
-            const $resizer = $(event.target)
-            const $parent = $resizer.closest('[data-type="resizable"]')
-            const coords = $parent.getCoords()
-            const type = $resizer.data.resize
-
-            const colIndex = $parent.data.index
-            const rows = colIndex ? this.$root.findAll(
-                '[data-row-data="cells"] div:nth-child(' + colIndex + ')'
-            ) : ''
-
-            if (colIndex) {
-                rows.forEach((cell) => {
-                    cell.style.borderRight = 2 + 'px solid #3c74ff'
-                })
-            }
-
-            document.onmousemove = e => {
-                $resizer.css({
-                    opacity: 1
-                })
-
-                if (type === 'col') {
-                    const delta = Math.floor(e.pageX - coords.right)
-                    this.width = coords.width + delta
-                    $resizer.css({
-                        right: -delta + 'px'
-                    })
-                } else {
-                    const delta = Math.floor(e.pageY - coords.bottom)
-                    this.height = coords.height + delta
-                    $parent.css({
-                        borderBottom: 2 + 'px solid #3c74ff'
-                    })
-                    $resizer.css({
-                        bottom: -delta + 'px'
-                    })
-                }
-            }
-
-            document.onmouseup = () => {
-                $resizer.css({
-                    opacity: ''
-                })
-
-                $parent.css({
-                    borderRight: '',
-                    borderBottom: ''
-                })
-                if (colIndex) {
-                    $parent.css({
-                        width: this.width + 'px'
-                    })
-                    rows.forEach((cell) => {
-                        cell.style.width = this.width + 'px'
-                        cell.style.borderRight = ''
-                    })
-                } else {
-                    $parent.css({
-                        height: this.height + 'px'
-                    })
-                }
-
-                $resizer.css({
-                    right: '',
-                    bottom: ''
-                })
-
-                document.onmousemove = null
-            }
+        if (shouldResize(event)) {
+            resize(event, this)
         }
     }
 }
---
+++ src/components/table/table.functions.js
@@ -0,0 +1,6 @@
+/*
+ * Check if dataset includes col/row meta
+ * */
+export function shouldResize(event) {
+    return event.target.dataset.resize
+}
