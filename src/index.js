abcfe3959765f73811ec019e6f3bf412b0ffd107
elvis97rus <elvis97rus@gmail.com>
2021-02-04 15:16:03 +0300

    Table logic. Selection, outline, navigation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import './scss/index.scss'
 import {Excel} from '@/components/excel/Excel';
 import {Header} from '@/components/header/Header';
 import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
+import {CreateStore} from '@core/CreateStore';
+import {rootReducer} from '@/redux/rootReducer';
+import './scss/index.scss'
+
+const store = new CreateStore(rootReducer)
 
 const excel = new Excel('#app', {
-    components: [Header, Toolbar, Formula, Table]
+    components: [Header, Toolbar, Formula, Table],
+    store
 })
 
 excel.render()
