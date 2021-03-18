9d0e9c9cc4f744753a30f0ecc799c48968b165cc
elvis97rus <elvis97rus@gmail.com>
2021-03-18 15:59:21 +0300

    Redux, Component state, localStorage - save 'Table' component state, cell width object dispatch - save row & col width/height state after page refresh - subscription logic optimisation - toolbar creation (state, logic) - toolbar state saving, default styles constant added, plural cell state change by toolbar btns - cell style status saving, cell&toolbar status linked, toolbar highlight after refresh, save header after refresh - debounce to prevent spam (in db for ex.) of state changes

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import {Table} from '@/components/table/Table';
 import {CreateStore} from '@core/CreateStore';
 import {rootReducer} from '@/redux/rootReducer';
 import './scss/index.scss'
-import {storage} from '@core/utils';
+import {storage, debounce} from '@core/utils';
 import {initialState} from '@/redux/initialState';
 
 const store = new CreateStore(rootReducer, initialState)
 
-store.subscribe(state => {
-    // console.log('App State: ', state)
-    storage('excel-state', state)
-})
+// prevent each time state change spam
+const stateListener = debounce(state => {
+        // console.log('App State: ', state)
+        storage('excel-state', state)
+    }, 300)
+
+store.subscribe(stateListener)
 
 const excel = new Excel('#app', {
     components: [Header, Toolbar, Formula, Table],
